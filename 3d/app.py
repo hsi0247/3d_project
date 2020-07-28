a1c335c0e71cabde88fd90df92c94b32f432cac2
jee9894 <43975730+jee9894@users.noreply.github.com>
2020-07-28 11:19:57 +0900

    파이썬 상대경로 모듈 import 방법

--- 3d/app.py
+++ 3d/app.py
@@ -3,6 +3,17 @@ import pymysql,os
 
 app=Flask(__name__)
 
+if __name__ == '__main__':
+    if __package__ is None:
+        import sys
+        from os import path
+        print(path.dirname( path.dirname( path.abspath(__file__) ) ))
+        sys.path.append(path.dirname( path.dirname( path.abspath(__file__) ) ))
+        # from function.모듈이름 import 함수이름
+    else:
+        # from ..function.모듈이름 import 함수이름
+        pass
+
 @app.route('/')
 def index():
     return render_template('index.html')
